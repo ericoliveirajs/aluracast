7f9afb8e7089d41dcc295ea216d023c08ae9dea9
Eric Oliveira <ericoliveirajs@gmail.com>
2025-10-21 12:44:16 -0300

    Configurando episodes.module.ts para que utilize TypeORM e a Entidade Episode

--- aluracast-backend/src/episodes/episodes.module.ts
+++ aluracast-backend/src/episodes/episodes.module.ts
@@ -1,8 +1,17 @@
+// aluracast-backend/src/episodes/episodes.module.ts
+
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm'; // 👈 NOVO: Importa o módulo do TypeORM
 import { EpisodesService } from './episodes.service';
 import { EpisodesController } from './episodes.controller';
+import { Episode } from './entities/episode.entity'; // 👈 NOVO: Importa a Entidade
 
 @Module({
+  imports: [
+    // 👈 NOVO: Registra a Entidade Episode para este módulo.
+    // Isso injeta o Repositório no EpisodesService.
+    TypeOrmModule.forFeature([Episode]) 
+  ],
   controllers: [EpisodesController],
   providers: [EpisodesService],
 })
