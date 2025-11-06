f30e90d9563134c896e970519cfd4b152024d70f
Eric Oliveira <ericoliveirajs@gmail.com>
2025-11-06 15:07:43 -0300

    feat(player): Implementa Player Context para estado global. Closes #23

---
+++ aluracast-frontend/src/contexts/PlayerContext.tsx
@@ -0,0 +1,52 @@
+import { createContext, useState, useContext, ReactNode } from 'react';
+import { Episode } from '@interfaces/episode.interface';
+
+interface PlayerContextData {
+  episode: Episode | null;
+  isPlaying: boolean;
+  selectEpisode: (episode: Episode) => void
+  play: () => void;
+  pause: () => void;
+}
+
+export const PlayerContext = createContext({} as PlayerContextData);
+
+interface PlayerProviderProps {
+  children: ReactNode;
+}
+
+export function PlayerProvider({ children }: PlayerProviderProps) {
+  const [episode, setEpisode] = useState<Episode | null>(null);
+  const [isPlaying, setIsPlaying] = useState(false);
+
+  function selectEpisode(episode: Episode) {
+    setEpisode(episode);
+    setIsPlaying(true);
+  }
+
+  function play() {
+    setIsPlaying(true);
+  }
+
+  function pause() {
+    setIsPlaying(false);
+  }
+
+  return (
+    <PlayerContext.Provider 
+      value={{ 
+        episode, 
+        isPlaying, 
+        selectEpisode, 
+        play, 
+        pause 
+      }}
+    >
+      {children}
+    </PlayerContext.Provider>
+  );
+}
+
+export const usePlayer = () => {
+  return useContext(PlayerContext);
+}
--- aluracast-frontend/src/pages/_app.tsx
+++ aluracast-frontend/src/pages/_app.tsx
@@ -1,6 +1,22 @@
-import "@/styles/globals.css";
-import type { AppProps } from "next/app";
+import type { AppProps } from 'next/app';
+import '@/styles/globals.css';
+import '@/styles/base.css';
+import '@/styles/grid/index.css';
+import '@/styles/responsivo.css';
+import '@/styles/grid/cartao.css';
+import '@/styles/grid/navbar.css';
+import '@/styles/grid/menu.css';
+import '@/styles/grid/rodape.css';
+import '@/styles/grid/secao.css';
+import '@/styles/Auth.module.css';
+
+
+import { PlayerProvider } from '@/contexts/PlayerContext';
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <PlayerProvider>
+      <Component {...pageProps} />
+    </PlayerProvider>
+  );
 }
